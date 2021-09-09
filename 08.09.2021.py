89f5ab00d842505c07daee07218340055c7f92f6
ponik <ponik555@inbox.ru>
2021-09-09 23:43:48 +0300

    second

---
+++ 08.09.2021.py
@@ -0,0 +1,13 @@
+import random
+gl = random.randint(4,8)
+I=chr (random.randint(65,90))
+
+def hf(num = 1):
+    for index in range(gl):
+        i=random.randint(97,122)
+        return chr(i)
+gg = 0
+while gg < gl:
+    I += hf(gl)
+    gg +=1
+print(I)
--- 08.09.21.py
+++ 08.09.21.py
@@ -1,6 +1,1 @@
-boy_next_door_=[]
-i=0
-while i<50:
-    boy_next_door_.append (i)
-    i=i+2
-print (*boy_next_door_,"50")
+print("heloo")
